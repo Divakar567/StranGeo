2d42d417740d4e4f07d34ecf2bae9d4a29e78402
LaserBolt567 <divakar.budumuru567@gmail.com>
2020-12-24 09:15:11 +0530

    conversations infinite rendering fixed

--- strangeo/src/app/requests.js
+++ strangeo/src/app/requests.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { generatePath } from "react-router";
 
-export const baseURL = "http://localhost:8080";
-export const conversationGetURL = "/api/conversations/{conversationId}";
+export const baseURL = "http://localhost:9091/api";
+export const conversationsGetURL = "/conversations";
+export const conversationPostURL = "/conversations";
+export const conversationGetURL = "/conversations/:conversationId";
 
 export const axiosConfig = {
     baseURL: baseURL,
@@ -10,12 +12,16 @@ export const axiosConfig = {
 }
 export const axiosInstance = axios.create(axiosConfig);
 
-export function getConversation(conversationId, authorization) {
+export const getAuthorization = (keycloak) => {
+    return (keycloak.tokenParsed.typ + " " + keycloak.token);
+}
+
+export function getConversation(conversationId, keycloak) {
     let config = {
         url: generatePath(conversationGetURL, { conversationId: conversationId }),
         method: 'get',
         headers: {
-            "Authorization": authorization,
+            "Authorization": getAuthorization(keycloak),
         },
 
     };
@@ -23,13 +29,42 @@ export function getConversation(conversationId, authorization) {
     return axiosInstance.request(config);
 }
 
-export function postConversation(data, token) {
-
+export function getConversations(page, size, keycloak) {
+    let config = {
+        url: conversationsGetURL,
+        method: 'get',
+        headers: {
+            "Authorization": getAuthorization(keycloak),
+            "Content-Type": 'application/json',
+        },
+        params: {
+            page: page,
+            size: size,
+        }
+    };
+    console.log("Request config: ", JSON.stringify(config));
+    return axiosInstance.request(config);
 }
 
-export default requests = {
+export function postConversation(data, keycloak) {
+    let config = {
+        url: conversationPostURL,
+        method: 'post',
+        headers: {
+            "Authorization": getAuthorization(keycloak),
+        },
+        data: data,
+    };
+
+    return axiosInstance.request(config);
+}
+
+const requests = {
     conversation: {
         get: getConversation,
+        list: getConversations,
         post: postConversation,
     }
 }
+
+export default requests;
